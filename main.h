9e6de58984da650f46290c57e63409f52da534ec
Draze52 <ryandrazek52@gmail.com>
2026-03-05 21:38:26 -0500

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,13 @@
+/*
+  Ryan Drazek, ID:123695485
+*/
 
+#ifndef _main_h
+#define _main_h 1
+
+#include "data_structures.h"
+
+void PrintArray(ELEMENT **V, int n);
+void PrintHeap(HEAP *pHeap, ELEMENT **V);
+
+#endif
