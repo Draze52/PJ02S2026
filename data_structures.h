2f5911f270c136ad86717889c1f0dc3560b39b7d
Draze52 <ryandrazek52@gmail.com>
2026-03-05 21:36:21 -0500

    Update data_structures.h

--- data_structures.h
+++ data_structures.h
@@ -2,19 +2,19 @@
   Ryan Drazek, ID:123695485
 */
 
-#ifndef DATA_STRUCTURES_H
-#define DATA_STRUCTURES_H
+#ifndef _data_structures_h
+#define _data_structures_h 1
 
 typedef struct TAG_ELEMENT{
-  int index;
-  double key;
-  int pos;
+	int index;
+	double key;
+	int pos;
 } ELEMENT;
 
 typedef struct TAG_HEAP{
-  int capacity;
-  int size;
-  int *H;
+	int capacity;
+	int size;
+	int *H;
 } HEAP;
 
 #endif
